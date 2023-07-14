f5b6927cce68e1cb4b2227094c3bb1865fa01b90
Simon Hoffmann <63074808+Simon-Hoffmann@users.noreply.github.com>
2023-07-14 11:18:45 +0200

    comment revision

--- delay.s
+++ delay.s
@@ -1,34 +1,7 @@
-  
-; ////////////////////////////////////
-; // SOURCE FILE   delay.s          //
-; ////////////////////////////////////
-  
-;/**************************************************************************
-; *  Eingebettete Betriebssystem    (EBSy)                                 *                                                                                                  *
-; **************************************************************************
-; *  PROJECT       Basisproject                                            *
-; *  MODULE        delay.s                                                 *
-; *  REVISION      2.0                                                     *
-; *  LAST AUTHOR                                                           *
-; *  LAST MODIFIED                                                         *
-; *  RESPONSIBLE   Peter Raab                                              *
-; **************************************************************************
-; *  PURPOSE:                                                              *
-; *   Delay of CPU execution; implements an exact time deley of R0 * 1ms   *
-; *                                                                        *
-; **************************************************************************
-; *  REMARKS:                                                              *
-; *   SYSCLK = 48 MHz                                                      *
-; *                                                                        *
-; **************************************************************************
-; *  CHANGE HISTORY:                                                       *
-; *  Revision   Date          Author      Description                      *
-; *     1.0      06.09.2019    P. Raab     creation                        *
-; *     2.0      09.09.2021    P. Raab     port to STM32G4                 *
-; *                                                                        *
-; *************************************************************************/
-  
-  
+; file: 	SchedulerASM.s
+; author:	Simon Hoffmann
+; brief: 	Delay of CPU execution; implements an exact time deley of R0 * 1ms
+
   AREA  DELAY, CODE, READONLY    
 
   THUMB
